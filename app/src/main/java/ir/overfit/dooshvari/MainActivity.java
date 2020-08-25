c8cd0000370f6c662bcba477de2126bb0b380dc5
aliakbarbadri <aabadri74@gmail.com>
2020-08-25 11:17:49 +0430

    Add project files

---
+++ app/src/main/java/ir/overfit/dooshvari/MainActivity.java
@@ -0,0 +1,17 @@
+package ir.overfit.dooshvari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "This is my Toast message!",
+                Toast.LENGTH_LONG).show();
+    }
+}
